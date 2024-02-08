841575532aac1cbdc8334e3d5be328b2a5515755
DiceOfDespair <155188448+DiceOfDespair@users.noreply.github.com>
2024-02-08 19:25:45 +0100

    Update a.js

--- a.js
+++ a.js
@@ -2,4 +2,5 @@ button = document.getElementById("#button1");
 
 button.addEventListener("click", (event) => {
       window.Open("https://gpu.userbenchmark.com/Compare/Nvidia-RTX-4070-S-Super-vs-Nvidia-RTX-3060-Ti/4154vs4090")
+      console.log("yes")
 });
