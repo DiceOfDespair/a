bab36eb95c039c3bfade8d71df43e0ec61c93b62
DiceOfDespair <155188448+DiceOfDespair@users.noreply.github.com>
2024-02-08 19:25:01 +0100

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,5 @@
-steam://run/2073850
+button = document.getElementById("#button1");
+
+button.addEventListener("click", (event) => {
+      window.Open("https://gpu.userbenchmark.com/Compare/Nvidia-RTX-4070-S-Super-vs-Nvidia-RTX-3060-Ti/4154vs4090")
+});
